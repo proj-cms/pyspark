67d4891dfad017a7ed1c9197993a85f0b49d82c2
Makarand Deshpande <deshpande.md@gmail.com>
2021-09-27 22:40:47 +0530

    Code for writing to kafka sink

--- WriteToKafkaSink/ReadKafkaSunkToKafka.py
+++ WriteToKafkaSink/ReadKafkaSunkToKafka.py
@@ -61,9 +61,56 @@ if __name__ == "__main__":
     # the column of importance is value column
     kafka_stream_df.printSchema()
 
+        '''
+        Scehma of Kafka read msg
+        root
+         |-- key: binary (nullable = true)
+         |-- value: binary (nullable = true)
+         |-- topic: string (nullable = true)
+         |-- partition: integer (nullable = true)
+         |-- offset: long (nullable = true)
+         |-- timestamp: timestamp (nullable = true)
+         |-- timestampType: integer (nullable = true)
+        '''
+
     value_df = kafka_stream_df.select(from_json(col("value").cast("string"), stream_invoice_schema).alias("value"))
 
     value_df.printSchema()
+
+    '''
+    schema generated after reading from value field in kafka streaming
+    root
+         |-- value: struct (nullable = true)
+         |    |-- InvoiceNumber: string (nullable = true)
+         |    |-- CreatedTime: long (nullable = true)
+         |    |-- StoreID: string (nullable = true)
+         |    |-- PosID: string (nullable = true)
+         |    |-- CashierID: string (nullable = true)
+         |    |-- CustomerType: string (nullable = true)
+         |    |-- CustomerCardNo: string (nullable = true)
+         |    |-- TotalAmount: double (nullable = true)
+         |    |-- NumberOfItems: integer (nullable = true)
+         |    |-- PaymentMethod: string (nullable = true)
+         |    |-- CGST: double (nullable = true)
+         |    |-- SGST: double (nullable = true)
+         |    |-- CESS: double (nullable = true)
+         |    |-- DeliveryType: string (nullable = true)
+         |    |-- DeliveryAddress: struct (nullable = true)
+         |    |    |-- AddressLine: string (nullable = true)
+         |    |    |-- City: string (nullable = true)
+         |    |    |-- State: string (nullable = true)
+         |    |    |-- PinCode: string (nullable = true)
+         |    |    |-- ContactNumber: string (nullable = true)
+         |    |-- InvoiceLineItems: array (nullable = true)
+         |    |    |-- element: struct (containsNull = true)
+         |    |    |    |-- ItemCode: string (nullable = true)
+         |    |    |    |-- ItemDescription: string (nullable = true)
+         |    |    |    |-- ItemPrice: double (nullable = true)
+         |    |    |    |-- ItemQty: integer (nullable = true)
+         |    |    |    |-- TotalValue: double (nullable = true)
+    '''
+
+
 
     exploded_flattened_df = value_df \
         .withColumn("LineItemExplode", explode(value_df["value"].getItem("InvoiceLineItems"))) \
@@ -90,7 +137,32 @@ if __name__ == "__main__":
 
     exploded_flattened_df.printSchema()
 
-    # Create the DF for notification
+    '''
+    exploded flattened schema
+    root
+     |-- InvoiceNumber: string (nullable = true)
+     |-- CreatedTime: long (nullable = true)
+     |-- StoreID: string (nullable = true)
+     |-- CustomerType: string (nullable = true)
+     |-- CustomerType: string (nullable = true)
+     |-- CustomerCardNo: string (nullable = true)
+     |-- TotalAmount: double (nullable = true)
+     |-- PaymentMethod: string (nullable = true)
+     |-- DeliveryType: string (nullable = true)
+     |-- AddressLine: string (nullable = true)
+     |-- City: string (nullable = true)
+     |-- ContactNumber: string (nullable = true)
+     |-- PinCode: string (nullable = true)
+     |-- State: string (nullable = true)
+     |-- ItemCode: string (nullable = true)
+     |-- ItemDescription: string (nullable = true)
+     |-- ItemPrice: double (nullable = true)
+     |-- ItemQty: integer (nullable = true)
+     |-- TotalValue: double (nullable = true)
+
+    '''
+
+    # Create the DF for notification with required columns
     notification_df = exploded_flattened_df \
         .select("InvoiceNumber"
                 , "CustomerCardNo"
