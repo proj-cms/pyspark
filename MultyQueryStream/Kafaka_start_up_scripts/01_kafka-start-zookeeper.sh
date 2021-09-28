8226edfca66492b4c5ef681f3e7239fa93e9c793
Makarand Deshpande <deshpande.md@gmail.com>
2021-09-28 15:00:55 +0530

    Mulit sink code

---
+++ MultyQueryStream/Kafaka_start_up_scripts/01_kafka-start-zookeeper.sh
@@ -0,0 +1,3 @@
+export KAFKA_HOME=/Users/makaranddeshpande/kafka212/kafka_2.12-3.0.0
+export PATH=${PATH}:${KAFKA_HOME}/bin
+zookeeper-server-start.sh ${KAFKA_HOME}/config/zookeeper.properties
---
+++ MultyQueryStream/Kafaka_start_up_scripts/05_kafka-start-producer.sh
@@ -0,0 +1,6 @@
+export KAFKA_HOME=/Users/makaranddeshpande/kafka212/kafka_2.12-3.0.0
+export PATH=${PATH}:${KAFKA_HOME}/bin
+kafka-console-producer.sh \
+--topic invoices \
+--bootstrap-server localhost:9092
+
