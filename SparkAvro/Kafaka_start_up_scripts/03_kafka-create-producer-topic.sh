39b553d04d7c1674ccae615cbf83b9fe442f1b9d
Makarand Deshpande <deshpande.md@gmail.com>
2021-09-28 20:38:45 +0530

    Avro write to Kafka

---
+++ SparkAvro/Kafaka_start_up_scripts/03_kafka-create-producer-topic.sh
@@ -0,0 +1,9 @@
+export KAFKA_HOME=/Users/makaranddeshpande/kafka212/kafka_2.12-3.0.0
+export PATH=${PATH}:${KAFKA_HOME}/bin
+kafka-topics.sh \
+--create \
+--topic invoices_1 \
+--bootstrap-server localhost:9092 \
+--partitions 2 \
+--replication-factor 1
+
