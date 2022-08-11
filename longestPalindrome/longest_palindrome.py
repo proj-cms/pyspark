def318976be6685029e01e0683fe57404f21c599
Makarand Deshpande <deshpande.md@gmail.com>
2022-08-11 18:53:21 +0530

    added python code for longest palindrome. Special credit youtube video from Nick White for explaining it so simple

---
+++ longestPalindrome/longest_palindrome.py
@@ -0,0 +1,33 @@
+def expandFromCenter(s, left, right):
+    if s is None or len(s) == 1:
+        return s
+
+    while left >= 0 and right < len(s) and s[left] == s[right]:
+        left -= 1
+        right += 1
+
+    return right - left - 1
+
+
+def callExpandFromCenter(s):
+    if s is None or len(s) == 1:
+        return s
+
+    start = 0
+    end = 0
+
+    for i in range(len(s)):
+        len1 = expandFromCenter(s, i, i)
+        len2 = expandFromCenter(s, i, i + 1)
+        len3 = max(len1, len2)
+
+        if len3 > (end - start):
+            start = i - (len3 - 1) // 2
+            end = i + len3 // 2
+
+    return s[start:end + 1]
+
+
+if __name__ == "__main__":
+    s = "abbbbbba"
+    print(callExpandFromCenter(s))
