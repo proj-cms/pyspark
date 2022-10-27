c9159b07871191d839219f2b824e35d32c72dda9
Makarand Deshpande <deshpande.md@gmail.com>
2022-10-27 17:12:52 +0530

    first missing positive number code , way 2

--- miscellaneous_code/firstMissingPositive_II.py
+++ miscellaneous_code/firstMissingPositive_II.py
@@ -5,7 +5,6 @@ def firstMissingPositiveII(arr: List[int]) -> int:
     n = len(arr)
     for i in range(0, len(arr)):
         val = arr[i]
-
         if 1 <= val < n:
             chair = val - 1
             if arr[i] != arr[chair]:
@@ -21,6 +20,7 @@ def firstMissingPositiveII(arr: List[int]) -> int:
 
 if __name__ == "__main__":
     # A = [3,-3,6,3]
-    A = [1, 2, 3]
+    # A = [1, 2, 3]
     # A = [-3, -5, 0]
+    A = [100, 200, 300]
     print(firstMissingPositiveII(A))
