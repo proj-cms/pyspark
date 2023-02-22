67a188f209e7041f109196132604c4b676254be5
Makarand Deshpande <deshpande.md@gmail.com>
2023-02-22 10:18:51 +0530

    fnew code

---
+++ miscellaneous_code/all_binary_strings_nbits.py
@@ -0,0 +1,17 @@
+import sys
+
+
+def binary(n: int):
+    if n < 1:
+        print(A)
+    else:
+        A[n - 1] = 0
+        binary(n - 1)
+        A[n - 1] = 1
+        binary(n - 1)
+
+
+if __name__ == "__main__":
+    print('Python %s on %s' % (sys.version, sys.platform))
+    A = [1, 2, 3, 4]
+    binary(4)
